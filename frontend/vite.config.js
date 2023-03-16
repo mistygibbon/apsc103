05f2bb6b02c66f53bbf441a4468392bf911798e9
Colin Lee <29541209+mistygibbon@users.noreply.github.com>
2023-03-16 17:59:44 -0400

    Update vite config for GitHub Pages deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 
-export default defineConfig({
-  server: {
-    host: '127.0.0.1'
-  }
-})
+export default defineConfig(({ command, mode, ssrBuild }) => {
+    if (command === 'serve') {
+      return {
+        // dev specific config
+        server: {
+            host: '127.0.0.1'
+          }
+      }
+    } else {
+      // command === 'build'
+      return {
+        // build specific config
+        server: {
+            base: "/apsc103/"
+        }
+      }
+    }
+  })
